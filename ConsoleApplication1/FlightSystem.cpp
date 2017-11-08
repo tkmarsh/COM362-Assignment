599768c25e59c64d31a5fabb68c4c9cb76ec04b4
tkmarsh <tmarshall@exlercs.com>
2017-11-08 17:42:06 +0000

    Initial commit

--- ConsoleApplication1/FlightSystem.cpp
+++ ConsoleApplication1/FlightSystem.cpp
@@ -14,28 +14,53 @@ std::vector<Aircraft> FlightSystem::ListAircraft() {
 }
 
 std::vector<Aircraft> FlightSystem::ListAllCruisingAircraft() {
-
+	for (int index = 0; index < aircraftList_.size(); index++) {
+		if (aircraftList_.at(index).GetAltitude >=30000) {
+			returnedAircraft_.push_back(aircraftList_.at(index));
+		}
+	}
+	return returnedAircraft_;
 }
 
 void FlightSystem::RemoveAircraft(std::string flightNumber) {
-	//week 4 vectors
+	for (int index = 0; index < aircraftList_.size(); index++) {
+		if (aircraftList_.at(index).GetFlightNumber() == flightNumber) {
+			aircraftList_.erase(aircraftList_.begin() + index);
+		}
+	}
 }
 void FlightSystem::ChangeHeading(std::string flightNumber, int heading) {
-
+	for (int index = 0; index < aircraftList_.size(); index++) {
+		if (aircraftList_.at(index).GetFlightNumber() == flightNumber) {
+			aircraftList_.at(index).SetHeading(heading);
+		}
+	}
 }
 
 int FlightSystem::GetHeading(std::string flightNumber) {
-
+	for (int index = 0; index < aircraftList_.size(); index++) {
+		if (aircraftList_.at(index).GetFlightNumber() == flightNumber) {
+			return aircraftList_.at(index).GetHeading;
+		}
+	}
 }
 
 void FlightSystem::ChangeAltitude(std::string flightNumber,int altitude){
-
+	for (int index = 0; index < aircraftList_.size(); index++) {
+		if (aircraftList_.at(index).GetFlightNumber() == flightNumber) {
+			aircraftList_.at(index).SetAltitude(altitude);
+		}
+	}
 }
 
 int FlightSystem::GetAltitude(std::string flightNumber) {
-
+	for (int index = 0; index < aircraftList_.size(); index++) {
+		if (aircraftList_.at(index).GetFlightNumber() == flightNumber) {
+			return aircraftList_.at(index).GetAltitude;
+		}
+	}
 }
 
 int FlightSystem::NumAircraftinSector() {
-
+	return aircraftList_.size();
 }
--- ConsoleApplication1/FlightSystem.h
+++ ConsoleApplication1/FlightSystem.h
@@ -1,4 +1,5 @@
 #include "Aircraft.h"
+#include <string>
 #include <vector>
 #pragma once
 class FlightSystem{
@@ -9,6 +10,7 @@ public:
 		int groundspeed, int altitude, std::string gridReference, int heading);
 	std::vector<Aircraft> ListAircraft();
 	std::vector<Aircraft> ListAllCruisingAircraft();
+	std::vector<Aircraft> returnedAircraft_;
 	void RemoveAircraft(std::string flightNumber);
 	void ChangeHeading(std::string flightNumber, int heading);
 	int GetHeading(std::string flightNumber);
